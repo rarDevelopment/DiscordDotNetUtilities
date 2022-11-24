306be4f9feba57ba002b1c8a3edce0efab320d55
Robert R <rarDevelopment@users.noreply.github.com>
2022-11-24 18:27:28 -0500

    added missing interface method

--- DiscordDotNetUtilities/Interfaces/IDiscordFormatter.cs
+++ DiscordDotNetUtilities/Interfaces/IDiscordFormatter.cs
@@ -6,4 +6,7 @@ public interface IDiscordFormatter
 {
     Embed BuildRegularEmbed(string title, string messageText, IUser? user = null, IList<EmbedFieldBuilder>? embedFieldBuilders = null, string url = "");
     Embed BuildErrorEmbed(string title, string messageText, IUser? user = null, IList<EmbedFieldBuilder>? embedFieldBuilders = null, string url = "");
+
+    IReadOnlyList<string> BuildMessageListFromStringList(IReadOnlyList<string> messagesToCombine, int maxMessageLength,
+        string firstMessageTitle = "", int newLinesAfterFirstMessage = 0);
 }
